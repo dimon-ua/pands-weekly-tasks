380667f9b4ef59aaf54344de25356808de619c9b
unknown <dima.kozlovskyy@gmail.com>
2025-02-18 17:01:52 +0000

    week04

---
+++ week04-flow/lab4.1.1_isEven.py
@@ -0,0 +1,6 @@
+number = int(input("Enter an integer: "))
+
+if(number % 2) == 0:
+    print("{}".format("is an even number"))
+else:
+    print("{}".format(" is an odd umber"))
---
+++ week04-flow/lab4.1.4_while_extra.py
@@ -0,0 +1,5 @@
+user_input = input("Enter a number: ")
+
+while user_input != str(-1):
+    print("If you want to quit, please enter -1 ")
+    user_input = input("Enter a number: ")
