3ea6445041eb364cec8d768ad9237220a44096f8
dimdev <dima.kozlovskyy@gmail.com>
2025-04-25 16:18:14 +0100

    Update weekday.py

--- Weekly Tasks 05/weekday.py
+++ Weekly Tasks 05/weekday.py
@@ -1,11 +1,16 @@
 # Importing datetime module
 import datetime
 
+# creating a list with working days
 weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 
+# datetime.datetime.now() will output a date in a format 2025-04-25 15:14:37.751113
+# to get a day but its name we need to add strftime("%A") method with %A argument to get the weekday - Monday, Tuesday, etc
 today_day = datetime.datetime.now().strftime("%A")
 
+# if/else statment, if Mon-Fri is in weekdays list. This is workday
 if today_day in weekdays:
     print("Yes, unfortunately today is a weekday.")
+    # if not, weekday
 else:
     print("It is the weekend, yay!")
