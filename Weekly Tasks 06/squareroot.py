5da7689823e49dc6d86b7ec16d75a984b2b9a9fb
dimdev <dima.kozlovskyy@gmail.com>
2025-04-25 16:21:52 +0100

    Update squareroot.py

--- Weekly Tasks 06/squareroot.py
+++ Weekly Tasks 06/squareroot.py
@@ -1,7 +1,10 @@
+# By the task of this program we neet to write a func to find a square root number not using build in methos
+
+# creating a func with n parameter
 def sqrt(n):
     x = 3
     approx = 0.5 * n
-
+# an iteration where in three steps finding the square root number with some Math steps
     for i in range(x):
         better_approx = 0.5*(approx + n/approx)
         approx = better_approx 
