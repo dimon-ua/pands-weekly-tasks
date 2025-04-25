c9c39cda03476639e23f13b771483dce20847278
dimdev <dima.kozlovskyy@gmail.com>
2025-04-25 15:54:04 +0100

    Update bank.py

--- Weekly Tasks 02/bank.py
+++ Weekly Tasks 02/bank.py
@@ -1,11 +1,15 @@
 # Author Dima
 # Program: asks the user to input two numbers and then prints it in human readable format
 
-amount1 = input("Please enter first amount(in cent) ")
+# asking a user to input two numbers
+amount1 = input("Please enter first amount(in cent) ") 
 amount2 = input("Please enter second amount(in cent) ")
+
+# getting numbers into Integer type to make some Math with them 
 amount1 = int(amount1) / 100
 amount2 = int(amount2) / 100
 
 sum = amount1 + amount2
 
+# output in readable format with the Euro sign
 print(f'The sum of these is \N{euro sign}{sum}')
